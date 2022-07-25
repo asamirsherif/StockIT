a8ed8808bbf9ea0ab3f6e20f1d926f607dc18bd1
Ahmed Samir <a.samirsherif@gmail.com>
2022-07-25 22:13:48 +0200

    add user routes to API routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:api'])->group(function () {
+
+    // -------------- USERS ---------------- \\
+
+    Route::get('GetUserAuth', [UserController::Class,'getUserAuth']);
+    Route::resource('users', UserController::Class);
 });
