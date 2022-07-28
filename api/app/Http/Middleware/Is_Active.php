b13836de864a3555eba3d529a8246d4668554489
Ahmed Samir <a.samirsherif@gmail.com>
2022-07-29 00:52:48 +0200

    add middleware to check if user is active or not

---
+++ api/app/Http/Middleware/Is_Active.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Is_Active
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+        //If the status is not approved redirect to login 
+        if(Auth::check() && !Auth::user()->status){
+            return redirect('/api/logout')->with('erro_login', 'Your User is no longer active, Contact the admin');
+        }
+        return $response;
+    }
+}
