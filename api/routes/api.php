c5d3040ac1a4c075325ccc28d2a078565caf0bb2
Ahmed Samir <a.samirsherif@gmail.com>
2022-07-29 02:05:55 +0200

    comment cors header in api (not currently needed)

--- api/routes/api.php
+++ api/routes/api.php
@@ -16,9 +16,9 @@ use App\Http\Controllers\PermissionController;
 |
 */
 
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization'); 
+// header('Access-Control-Allow-Origin:  *');
+// header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization'); 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
