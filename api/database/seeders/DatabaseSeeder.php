db541e205c85df675df93b4af26eeb3e62327289
Ahmed Samir <a.samirsherif@gmail.com>
2022-08-01 05:07:53 +0200

    add client and currency seeders

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            ClientSeeder::class,
+            CurrencySeeder::class,
             PermissionSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
