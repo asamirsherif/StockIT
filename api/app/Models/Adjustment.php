e80e688b71bc4fd816e1ff780d7fa0c3ffb22b73
Ahmed fouad <ahfouad12@gmail.com>
2022-08-01 02:59:33 +0200

    adding adjustment relation in model

--- api/app/Models/Adjustment.php
+++ api/app/Models/Adjustment.php
@@ -13,4 +13,16 @@ class Adjustment extends Model
     protected $fillable = [
         'user_id', 'date', 'Ref', 'warehouse_id', 'items', 'notes'
     ];
+
+
+    // user_id relation
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    // warehouse_id relation
+    // public function warehouse()
+    // {
+    //     return $this->belongsTo(Warehouse::class);
+    // }
 }
