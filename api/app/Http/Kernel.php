64892db0a48fa79a3d3974e470a3c381d401f342
Ahmed Samir <a.samirsherif@gmail.com>
2022-08-07 02:49:02 +0200

    add cors to fix localhost integration issues (must be removed latet)

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'Is_Active' => \App\Http\Middleware\Is_Active::class,
+        'cors' => \App\Http\Middleware\Cors::class, 
     ];
 }
