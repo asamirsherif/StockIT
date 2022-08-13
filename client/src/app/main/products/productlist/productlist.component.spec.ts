fe7304d4c0953175a6b9c97ebd7bcb67dd681f6c
Ahmed fouad <ahfouad12@gmail.com>
2022-08-13 17:48:06 +0200

    no

--- client/src/app/main/products/productlist/productlist.component.spec.ts
+++ client/src/app/main/products/productlist/productlist.component.spec.ts
@@ -1,25 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ProductlistComponent } from './productlist.component';
+import { ProductlistComponent } from "./productlist.component";
 
-describe('ProductlistComponent', () => {
-  let component: ProductlistComponent;
-  let fixture: ComponentFixture<ProductlistComponent>;
+describe("ProductlistComponent", () => {
+    let component: ProductlistComponent;
+    let fixture: ComponentFixture<ProductlistComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProductlistComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ProductlistComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProductlistComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProductlistComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
