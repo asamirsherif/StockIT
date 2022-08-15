484adae154338948711844d41ea5d78f9d2dc7b1
Your samira <samiraelgendy6@gmail.com>
2022-08-15 23:36:08 +0200

    update customer validation

--- client/src/app/main/settings/currency/currency.component.ts
+++ client/src/app/main/settings/currency/currency.component.ts
@@ -7,13 +7,13 @@ import { Router } from "@angular/router";
 import { ToastrService } from "ngx-toastr";
 import { HttpErrorResponse } from "@angular/common/http";
 @Component({
-  selector: 'app-currency',
-  templateUrl: './currency.component.html',
-  styleUrls: ['./currency.component.scss']
+selector: 'app-currency',
+templateUrl: './currency.component.html',
+styleUrls: ['./currency.component.scss']
 })
 export class CurrencyComponent implements OnInit {
-  public pageBasicText = 3;
-  data:Array<Icurreny>=[];
+public pageBasicText = 3;
+data:Array<Icurreny>=[];
   currForEdit!: Icurreny;
 
   createcurrency:FormGroup;
@@ -24,98 +24,96 @@ export class CurrencyComponent implements OnInit {
 
   searchInput: string = "";
   constructor(private modalService: NgbModal ,private fb:FormBuilder,
-    private _curr: CurrencyService,
-    public _router: Router,
-    private toaster: ToastrService) { 
-    this.createcurrency = new FormGroup({
-      name: new FormControl("", Validators.required),
-    code: new FormControl(null),
-    symbol: new FormControl(null)});
-    this.editCurrForm = new FormGroup({
-      name: new FormControl("", Validators.required),
-      code: new FormControl(null),
-      symbol: new FormControl(null)
-      });
+  private _curr: CurrencyService,
+  public _router: Router,
+  private toaster: ToastrService) {
+  this.createcurrency = new FormGroup({
+  name: new FormControl("", Validators.required),
+  code: new FormControl(null),
+  symbol: new FormControl(null)});
+  this.editCurrForm = new FormGroup({
+  name: new FormControl("", Validators.required),
+  code: new FormControl(null),
+  symbol: new FormControl(null)
+  });
   }
-  
+
   openModal(contentModal) {
-    this.contentModel = contentModal;
-    this.modalService.open(contentModal);
-    }
-    closeModel(contentModal) {
-      this.modalService.dismissAll(contentModal);
-      } 
-    openModal2(contentModal2) {
-      this.contentModel = contentModal2;
-      this.modalService.open(contentModal2);
-      }  
+  this.contentModel = contentModal;
+  this.modalService.open(contentModal);
+  }
+  closeModel(contentModal) {
+  this.modalService.dismissAll(contentModal);
+  }
+  openModal2(contentModal2) {
+  this.contentModel = contentModal2;
+  this.modalService.open(contentModal2);
+  }
   ngOnInit(): void {
-    this.AllData();
+  this.AllData();
   }
   AllData() {
-    this._curr.allcurr().subscribe((res) => {
-    this.data = res.data;
-    });
-    }
-    AddCurr() {
-      this.submitted = true;
-    if (this.createcurrency.valid) {
-    const observer = {
-    next: (res) => {
-    this.closeModel(this.contentModel);
-    this.toaster.success(res.message);
-    this.data.push(res.data);
-    },
-    error: (error: HttpErrorResponse) => {
-    console.log(error.status);
-    },
-    };
-    this._curr.AddCurr(this.createcurrency.value).subscribe(observer);
-    }
-    }
+  this._curr.allcurr().subscribe((res) => {
+  this.data = res.data;
+  });
+  }
+  AddCurr() {
+  this.submitted = true;
+  if (this.createcurrency.valid) {
+  const observer = {
+  next: (res) => {
+  this.closeModel(this.contentModel);
+  this.toaster.success(res.message);
+  this.data.push(res.data);
+  }, error: (error: HttpErrorResponse) => {
+  this.errors = error.error.errors;
+  },
+  };
+  this._curr.AddCurr(this.createcurrency.value).subscribe(observer);
+  }
+  }
 
-    editCurr(id: number) {
-      const observer = {
-      next: (res) => {
-      this.currForEdit = res.data;
-      console.log(this.currForEdit);
-    
-      this.editCurrForm.get("name").setValue(this.currForEdit.name);
-      this.editCurrForm.get("code").setValue(this.currForEdit.code);
-      this.editCurrForm.get("symbol").setValue(this.currForEdit.symbol);
-      },
-      error: (error) => {
-      console.log(error);
-      },
-      };
-    
-      this._curr.getCurrid(id).subscribe(observer);
-      }
-      updateCurr() {
-       
-        const observer = {
-        next: (res) => {this.closeModel(this.contentModel);
-        this.AllData();
-        },
-        error: (error) => {
-        console.log(error);
-        },
-        };
-        this._curr.updateCurr(this.currForEdit?.id, this.editCurrForm.value)
-        .subscribe(observer);
-        }
-      
-        deleteCurr(id: any) {
-        console.log(id);
-        this._curr.deleteCurr(id).subscribe((res) => {
-        this.AllData();
-        });
-        }
-      
-        search(event) {
-      
-        this._curr.params = this._curr.params.set("search", event);
-      
-        this.AllData();
-        }   
-}
+  editCurr(id: number) {
+  const observer = {
+  next: (res) => {
+  this.currForEdit = res.data;
+  console.log(this.currForEdit);
+
+  this.editCurrForm.get("name").setValue(this.currForEdit.name);
+  this.editCurrForm.get("code").setValue(this.currForEdit.code);
+  this.editCurrForm.get("symbol").setValue(this.currForEdit.symbol);
+  },
+  error: (error) => {
+  console.log(error);
+  },
+  };
+
+  this._curr.getCurrid(id).subscribe(observer);
+  }
+  updateCurr() {
+
+  const observer = {
+  next: (res) => {this.closeModel(this.contentModel);
+  this.AllData();
+  }, error: (error: HttpErrorResponse) => {
+  this.errors = error.error.errors;
+  },
+  };
+  this._curr.updateCurr(this.currForEdit?.id, this.editCurrForm.value)
+  .subscribe(observer);
+  }
+
+  deleteCurr(id: any) {
+  console.log(id);
+  this._curr.deleteCurr(id).subscribe((res) => {
+  this.AllData();
+  });
+  }
+
+  search(event) {
+
+  this._curr.params = this._curr.params.set("search", event);
+
+  this.AllData();
+  }
+  }
