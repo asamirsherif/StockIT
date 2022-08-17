9c6ccc17e07575917ea30c1229da36ddf6d4e011
ammaragha <ammaraghaagha@gmail.com>
2022-08-17 17:34:12 +0200

    finished create purchase with all validation ,toastrs and fix purcahse request for add product to warehouse

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import 'hammerjs';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { ToastrModule } from 'ngx-toastr'; // For auth after login toast
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { CoreModule } from '@core/core.module';
 import { CoreCommonModule } from '@core/common.module';
 import { CoreSidebarModule, CoreThemeCustomizerModule } from '@core/components';
@@ -20,7 +20,7 @@ import { LayoutModule } from 'app/layout/layout.module';
 import { SampleModule } from 'app/main/sample/sample.module';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http'
-import {  AuthGuard, ErrorInterceptor, JwtInterceptor } from 'app/auth/helpers';
+import { AuthGuard, ErrorInterceptor, JwtInterceptor } from 'app/auth/helpers';
 
 import { CreateproductComponent } from './main/products/createproduct/createproduct.component';
 import { ProductlistComponent } from './main/products/productlist/productlist.component';
@@ -86,103 +86,103 @@ const appRoutes: Routes = [
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
-  },{
+  }, {
     path: 'createproduct',
     canActivate: [AuthGuard],
-    data: { permission: 'products_add'},
+    data: { permission: 'products_add' },
     loadChildren: () => import('./main/products/createproduct/createproduct.module').then(m => m.CreateproductModule)
-  },{
+  }, {
     path: 'productlist',
     canActivate: [AuthGuard],
-    data: { permission: 'products_view'},
+    data: { permission: 'products_view' },
     loadChildren: () => import('./main/products/productlist/productlist.module').then(m => m.CreateproductModule)
-  },{
+  }, {
     path: 'printbarcode',
     canActivate: [AuthGuard],
-    data: { permission: 'barcode_view'},
+    data: { permission: 'barcode_view' },
     loadChildren: () => import('./main/products/printbarcode/printbarcode.module').then(m => m.CreateproductModule)
-  },{
+  }, {
     path: 'createadjustment',
     canActivate: [AuthGuard],
-    data: { permission: 'adjustment_add'},
+    data: { permission: 'adjustment_add' },
     loadChildren: () => import('./main/Adjustment/createadjustment/createadjustment.module').then(m => m.CreateadjustmentModule)
-  },{
+  }, {
     path: 'listadjustment',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Adjustment/listadjustment/listadjustment.module').then(m => m.ListadjustmentModule)
-  },{
+  }, {
     path: 'purchases',
     canActivate: [AuthGuard],
-    loadChildren: () => import('./main/Reports/Payments/purchases/purchases.module').then(m => m.PurchasesModule )
-  },{
+    loadChildren: () => import('./main/Reports/Payments/purchases/purchases.module').then(m => m.PurchasesModule)
+  }, {
     path: 'sales',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/Payments/sales/sales.module').then(m => m.SalesModule)
-  },{
+  }, {
     path: 'sales return',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/Payments/sales-return/sales-return.module').then(m => m.SalesReturnModule)
-  },{
+  }, {
     path: 'purchases return',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/Payments/purchases-return/purchases-return.module').then(m => m.PurchasesReturnModule)
-  },{
+  }, {
     path: 'profit and loss',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/profit-and-loss/profit-and-loss.module').then(m => m.ProfitAndLossModule)
-  },{
+  }, {
     path: 'product quantity alerts',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/prductquantity/productquantity.module').then(m => m.PrductquantityModule)
-  },{
+  }, {
     path: 'warehouse report',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/warehouse-report/warehouse.module').then(m => m.WarehouseReportModule)
-  },{
+  }, {
     path: 'sale report',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/sale-report/sale-report.module').then(m => m.SaleReportModule)
-  },{
+  }, {
     path: 'purchase report',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/purchase-report/purchase-report.module').then(m => m.PurchaseReportModule)
-  },{
+  }, {
     path: 'customer report',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/customer-report/customer-report.module').then(m => m.CustomerReportModule)
-  },{
+  }, {
     path: 'suplplier report',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Reports/supplier-report/supplier-report.module').then(m => m.SupplierReportModule)
-  },{
+  }, {
     path: 'system setting',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/settings/systemsetting/systemsetting.module').then(m => m.SystemsettingModule)
-  },{
+  }, {
     path: 'group permission',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/settings/grouppermission/grouppermission.module').then(m => m.GrouppermissionModule)
-  },{
+  }, {
     path: 'warehouse',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/settings/warehouse/warehouse.module').then(m => m.WarehouseReportModule)
-  },{
+  }, {
     path: 'category',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/settings/category/category.module').then(m => m.CategoryModule)
-  },{
+  }, {
     path: 'brand',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/settings/brand/brand.module').then(m => m.BrandModule)
-  },{
+  }, {
     path: 'currency',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/settings/currency/currency.module').then(m => m.CurrencyModule)
-  },{
+  }, {
     path: 'unit',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/settings/unit/unit.module').then(m => m.UnitModule)
-  },{
+  }, {
     path: 'backup',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/settings/backup/backup.module').then(m => m.BackupModule)
@@ -200,8 +200,8 @@ const appRoutes: Routes = [
   {
     path: 'createexpenses',
     canActivate: [AuthGuard],
-    loadChildren: () => import('./main/expenses/createexpenses/createexpenses.module').then(m => m.CreateexpensesModule )
-      
+    loadChildren: () => import('./main/expenses/createexpenses/createexpenses.module').then(m => m.CreateexpensesModule)
+
   },
   {
     path: 'expenseslist',
@@ -282,31 +282,31 @@ const appRoutes: Routes = [
     path: 'adjustupdate/:id',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Adjustment/adjustupdate/adjustupdate.module').then(m => m.AdjustupdateModule)
-  },{
+  }, {
     path: 'expenseupdate/:id',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/expenses/expenseupdate/expensesupdate.module').then(m => m.ExpenseupdateModule)
-  },{
+  }, {
     path: 'createpermission',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/setting/permission/createpermission/createpermission.module').then(m => m.CreatepermissionModule)
-  },{
+  }, {
     path: 'productdetails/:id',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/products/productdetatils/productdetails.module').then(m => m.ProductdetatilsModule)
-  },{
+  }, {
     path: 'productupdate/:id',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/products/productupdate/productupdate.module').then(m => m.ProductupdateComponentModule)
-  },{
+  }, {
     path: 'purchasesupdate',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/Purchases/purchasesupdate/purchasesupdate.module').then(m => m.PurchasesupdateModule)
-  },{
+  }, {
     path: 'quotationupdate',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/quotations/quotationupdate/quotationupdate.module').then(m => m.QuotationupdateModule)
-  },{
+  }, {
     path: 'salesupdate',
     canActivate: [AuthGuard],
     loadChildren: () => import('./main/sales/salesupdate/salesupdate.module').then(m => m.SalesupdateModule)
@@ -314,7 +314,7 @@ const appRoutes: Routes = [
   {
     path: 'permissionupdate',
     loadChildren: () => import('./main/settings/grouppermissingupdate/grouppermissionupdate.module').then(m => m.GrouppermissingupdateModule)
-  },{
+  }, {
     path: 'ternsupdate',
     loadChildren: () => import('./main/transfer/updatetransfer/updatetransfer.module').then(m => m.UpdatetransferModule)
   },
@@ -328,7 +328,9 @@ const appRoutes: Routes = [
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule, FormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes, {
@@ -358,4 +360,4 @@ const appRoutes: Routes = [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
   ]
 })
-export class AppModule {}
+export class AppModule { }
