c65cecd7337c734e6afa038afed76b7be891be42
asmaaabdelatif <asmaa.abdelatif98@gmail.com>
2022-08-18 15:03:26 +0200

    any

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to vuexy!');
+    expect(page.getTitleText()).toEqual('Welcome to StockIT!');
   });
 });
