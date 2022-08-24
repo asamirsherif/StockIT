304cb7f63cb96c5e0889eb364fbba6b9980be263
asmaaabdelatif <asmaa.abdelatif98@gmail.com>
2022-08-25 00:24:11 +0200

    test

--- client/src/app/main/Reports/profit-and-loss/profit-and-loss.component.ts
+++ client/src/app/main/Reports/profit-and-loss/profit-and-loss.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { Chart,registerables } from 'chart.js';
+import {NgbDate, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+
 Chart.register(...registerables)
 @Component({
   selector: 'app-profit-and-loss',
@@ -8,8 +10,38 @@ Chart.register(...registerables)
 })
 
 export class ProfitAndLossComponent implements OnInit {
+  hoveredDate: NgbDate | null = null;
 
-  constructor(private _elementRef: ElementRef) { }
+  fromDate: NgbDate;
+  toDate: NgbDate | null = null;
+
+  constructor(private _elementRef: ElementRef,calendar: NgbCalendar) { 
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
+  }
 
   ngOnInit(): void {
     let chartDiv = this._elementRef.nativeElement.querySelector('#pieChart')
