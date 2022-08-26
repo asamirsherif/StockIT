74b6d26e9451e495509dc2244dca813effcd6bb1
Ahmed fouad <ahfouad12@gmail.com>
2022-08-26 15:37:01 +0200

    add purchase policy

---
+++ api/app/Policies/PurchasePolicy.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Permission;
+use App\Models\Purchase;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PurchasePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Purchase  $purchase
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Purchase $purchase)
+    {
+        $permission = Permission::where('name', 'purchases_view')->first();
+        return $user->hasRole($permission->roles);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        $permission = Permission::where('name', 'purchases_add')->first();
+        return $user->hasRole($permission->roles);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Purchase  $purchase
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Purchase $purchase)
+    {
+        $permission = Permission::where('name', 'purchases_edit')->first();
+        return $user->hasRole($permission->roles);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Purchase  $purchase
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Purchase $purchase)
+    {
+        $permission = Permission::where('name', 'purchases_delete')->first();
+        return $user->hasRole($permission->roles);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Purchase  $purchase
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Purchase  $purchase
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Purchase $purchase)
+    {
+        //
+    }
+}
